5cc8dabd42ca3a1c379973cb8ec53965a2b3280a
Darshan Koirala <theanimatorspal@gmail.com>
2024-09-25 10:07:10 +0545

    sdg

---
+++ ElementLibrary/Commons/Require.lua
@@ -0,0 +1,9 @@
+require "Present.Present"
+require "ElementLibrary.colors"
+
+background_color = vec4(gcolors.flamingopink, 1)
+accent_color = vec4(0.6, 0.4, 0.5, 0.7)
+inverse_text_color = vec4(1)
+transparent_color = vec4(1, 1, 1, 0.8)
+very_transparent_color = vec4(1, 1, 1, 0.2)
+white_color = vec4(1)
