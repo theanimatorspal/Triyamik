1a7729fa34c216a51139f532aa1f82166ca91df4
Darshan Koirala <theanimatorspal@gmail.com>
2025-01-23 12:32:15 +0545

    Commit

--- ElementLibrary/Commons/PictureList.lua
+++ ElementLibrary/Commons/PictureList.lua
@@ -1,15 +1,34 @@
 CPictureList = function(inCPictureList)
     local t = {
-        path = -1,
+        paths = -1,
         p = vec3(100, 100, 1),
         d = vec3(100, 100, 1),
-        i = 1,
+        c = vec4(1),
+        index = 1,
     }
     return { CPictureList = Default(inCPictureList, t) }
 end
 
 gprocess.CPictureList = function(inPresentation, inValue, inFrameIndex, inElementName)
-    local pictureElementName = gUnique(inElementName)
-          
-          end
-
+    local elementName = gUnique(inElementName)
+    local paths = inValue.paths
+    local index = inValue.i
+    local p = inValue.p
+    local d = inValue.d
+    local c = inValue.c
+    if paths ~= -1 then
+        for i = 1, #paths do
+            local alpha = 0
+            if i == index then
+                alpha = 1
+            end
+            gprocess.CPicture(inPresentation, CPicture {
+                pic = paths[index],
+                p = p,
+                d = d,
+                c = vec4(c.x, c.y, c.z, c.w * alpha),
+                bc = vec4(c.x, c.y, c.z, c.w * alpha)
+            }.CPicture, inFrameIndex, elementName .. "__" .. paths)
+        end
+    end
+end
