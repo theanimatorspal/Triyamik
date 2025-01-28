09edb19f83bedf8310332c6b886c82efc244173c
dipesh7002 <077bct027.dipesh@pcampus.edu.np>
2025-01-28 09:52:04 +0545

    la commit hai ta

--- ElementLibrary/Procedurals/Require.lua
+++ ElementLibrary/Procedurals/Require.lua
@@ -2,93 +2,93 @@ require "Present.Present"
 require "ElementLibrary.Commons.Commons"
 PRO = {}
 PRO.GetCubeGenerator = function(sizex, sizey, sizez, type)
-          local c = Jkr.CustomShape3D()
-          local v3ds = std_vector_Vertex3D()
-          local uis = std_vector_uint()
-          local x = sizex;
-          local y = sizey;
-          local z = sizez;
-          local positions = {
-                    vec3(-x, -y, z),
-                    vec3(x, -y, z),
-                    vec3(x, -y, -z),
-                    vec3(-x, -y, -z),
-                    vec3(-x, y, z),
-                    vec3(x, y, z),
-                    vec3(x, y, -z),
-                    vec3(-x, y, -z),
-          }
-          local normals = {
-                    vec3(0, -1, 0),
-                    vec3(0, 1, 0),
-                    vec3(-1, 0, 0),
-                    vec3(0, 0, 1),
-                    vec3(0, 0, -1),
-                    vec3(0, 0, 1),
-          };
+    local c = Jkr.CustomShape3D()
+    local v3ds = std_vector_Vertex3D()
+    local uis = std_vector_uint()
+    local x = sizex;
+    local y = sizey;
+    local z = sizez;
+    local positions = {
+        vec3(-x, -y, z),
+        vec3(x, -y, z),
+        vec3(x, -y, -z),
+        vec3(-x, -y, -z),
+        vec3(-x, y, z),
+        vec3(x, y, z),
+        vec3(x, y, -z),
+        vec3(-x, y, -z),
+    }
+    local normals = {
+        vec3(0, -1, 0),
+        vec3(0, 1, 0),
+        vec3(-1, 0, 0),
+        vec3(0, 0, 1),
+        vec3(0, 0, -1),
+        vec3(0, 0, 1),
+    };
 
-          local uvs = {
-                    vec2(1, 1),
-                    vec2(0, 1),
-                    vec2(0, 0),
-                    vec2(1, 0),
-          }
+    local uvs = {
+        vec2(1, 1),
+        vec2(0, 1),
+        vec2(0, 0),
+        vec2(1, 0),
+    }
 
-          local faceIndices = {
-                    { 0, 1, 2, 3 },
-                    { 4, 5, 6, 7 },
-                    { 0, 1, 5, 4 },
-                    { 3, 2, 6, 7 },
-                    { 0, 3, 7, 4 },
-                    { 1, 2, 6, 5 }
-          }
-          for i = 1, 6 do
-                    for j = 1, 4 do
-                              local faceIndex = faceIndices[i][j]
-                              local v = Jkr.Vertex3D()
-                              v.mPosition = positions[faceIndex + 1]
-                              v.mNormal = normals[i]
-                              if type == "TWOSIDED" then
-                                        if i == 4 or i == 3 then
-                                                  v.mUV = uvs[j]
-                                        else
-                                                  v.mUV = vec2(0, 0)
-                                        end
-                              elseif type == "FOURSIDED" then
-                                        v.mUV = uvs[j]
-                              else
-                                        v.mUV = uvs[j]
-                              end
-                              v.mColor = vec3(1)
-                              v3ds:add(v)
-                    end
-          end
-          for i = 1, 6 do
-                    local offset = (i - 1) * 4
-                    uis:add(offset)
-                    uis:add(offset + 1)
-                    uis:add(offset + 2)
-                    uis:add(offset)
-                    uis:add(offset + 2)
-                    uis:add(offset + 3)
-          end
-          c.mVertices = v3ds
-          c.mIndices = uis
-          return Jkr.Generator(Jkr.Shapes.CustomShape3D, c)
+    local faceIndices = {
+        { 0, 1, 2, 3 },
+        { 4, 5, 6, 7 },
+        { 0, 1, 5, 4 },
+        { 3, 2, 6, 7 },
+        { 0, 3, 7, 4 },
+        { 1, 2, 6, 5 }
+    }
+    for i = 1, 6 do
+        for j = 1, 4 do
+            local faceIndex = faceIndices[i][j]
+            local v = Jkr.Vertex3D()
+            v.mPosition = positions[faceIndex + 1]
+            v.mNormal = normals[i]
+            if type == "TWOSIDED" then
+                if i == 4 or i == 3 then
+                    v.mUV = uvs[j]
+                else
+                    v.mUV = vec2(0, 0)
+                end
+            elseif type == "FOURSIDED" then
+                v.mUV = uvs[j]
+            else
+                v.mUV = uvs[j]
+            end
+            v.mColor = vec3(1)
+            v3ds:add(v)
+        end
+    end
+    for i = 1, 6 do
+        local offset = (i - 1) * 4
+        uis:add(offset)
+        uis:add(offset + 1)
+        uis:add(offset + 2)
+        uis:add(offset)
+        uis:add(offset + 2)
+        uis:add(offset + 3)
+    end
+    c.mVertices = v3ds
+    c.mIndices = uis
+    return Jkr.Generator(Jkr.Shapes.CustomShape3D, c)
 end
 
 
 PRO.CompileShaders = function()
-          if not PRO.cube_ShowImageShader then
-                    local vshader, fshader = Engine.GetAppropriateShader("NORMAL",
-                              Jkr.CompileContext.Default, nil, nil,
-                              nil, nil, { baseColorTexture = true })
-                    fshader = Basics.GetConstantFragmentHeader()
-                        .Append "layout(set = 0, binding = 32) uniform sampler2DArray shadowMap;\n"
-                        .uSampler2D(3, "uBaseColorTexture")
-                    PBR.PreCalcImages(fshader)
-                    fshader.GlslMainBegin()
-                        .Append [[
+    if not PRO.cube_ShowImageShader then
+        local vshader, fshader = Engine.GetAppropriateShader("NORMAL",
+            Jkr.CompileContext.Default, nil, nil,
+            nil, nil, { baseColorTexture = true })
+        fshader = Basics.GetConstantFragmentHeader()
+            .Append "layout(set = 0, binding = 32) uniform sampler2DArray shadowMap;\n"
+            .uSampler2D(3, "uBaseColorTexture")
+        PBR.PreCalcImages(fshader)
+        fshader.GlslMainBegin()
+            .Append [[
                     vec4 color = Push.m2[0];
                     vec4 background_color = Push.m2[1];
                     vec4 outC = texture(uBaseColorTexture, vUV);
@@ -102,34 +102,34 @@ PRO.CompileShaders = function()
                          outFragColor = background_color;
                     }
                     ]]
-                        .GlslMainEnd()
+            .GlslMainEnd()
 
-                    local simple3did = gworld3d:AddSimple3D(Engine.i, gwindow)
-                    local simple3d = gworld3d:GetSimple3D(simple3did)
-                    simple3d:CompileEXT(
-                              Engine.i,
-                              gwindow,
-                              "cache/3d_background_color_shader.glsl",
-                              vshader.str,
-                              fshader.str,
-                              "",
-                              false,
-                              Jkr.CompileContext.Default
-                    )
-                    PRO.cube_ShowImageShader = {}
-                    PRO.cube_ShowImageShader.simple3did = simple3did
-                    PRO.cube_ShowImageShader.simple3d = simple3d
-          end
-          if not PRO.cube_ShowImageShader then
-                    local vshader, fshader = Engine.GetAppropriateShader("NORMAL",
-                              Jkr.CompileContext.Default, nil, nil,
-                              nil, nil, { baseColorTexture = true })
-                    fshader = Basics.GetConstantFragmentHeader()
-                        .Append "layout(set = 0, binding = 32) uniform sampler2DArray shadowMap;\n"
-                        .uSampler2D(3, "uBaseColorTexture")
-                    PBR.PreCalcImages(fshader)
-                    fshader.GlslMainBegin()
-                        .Append [[
+        local simple3did = gworld3d:AddSimple3D(Engine.i, gwindow)
+        local simple3d = gworld3d:GetSimple3D(simple3did)
+        simple3d:CompileEXT(
+            Engine.i,
+            gwindow,
+            "cache/3d_background_color_shader.glsl",
+            vshader.str,
+            fshader.str,
+            "",
+            false,
+            Jkr.CompileContext.Default
+        )
+        PRO.cube_ShowImageShader = {}
+        PRO.cube_ShowImageShader.simple3did = simple3did
+        PRO.cube_ShowImageShader.simple3d = simple3d
+    end
+    if not PRO.cube_ShowImageShader then
+        local vshader, fshader = Engine.GetAppropriateShader("NORMAL",
+            Jkr.CompileContext.Default, nil, nil,
+            nil, nil, { baseColorTexture = true })
+        fshader = Basics.GetConstantFragmentHeader()
+            .Append "layout(set = 0, binding = 32) uniform sampler2DArray shadowMap;\n"
+            .uSampler2D(3, "uBaseColorTexture")
+        PBR.PreCalcImages(fshader)
+        fshader.GlslMainBegin()
+            .Append [[
                     vec4 color = Push.m2[0];
                     vec4 background_color = Push.m2[1];
                     vec4 outC = texture(uBaseColorTexture, vUV) + background_color;
@@ -143,32 +143,30 @@ PRO.CompileShaders = function()
                         outFragColor = background_color;
                     }
                     ]]
-                        .GlslMainEnd()
+            .GlslMainEnd()
 
-                    local simple3did = gworld3d:AddSimple3D(Engine.i, gwindow)
-                    local simple3d = gworld3d:GetSimple3D(simple3did)
-                    simple3d:CompileEXT(
-                              Engine.i,
-                              gwindow,
-                              "cache/3d_background_color_shader.glsl",
-                              vshader.str,
-                              fshader.str,
-                              "",
-                              false,
-                              Jkr.CompileContext.Default
-                    )
-                    PRO.cube_ShowImageShader = {}
-                    PRO.cube_ShowImageShader.simple3did = simple3did
-                    PRO.cube_ShowImageShader.simple3d = simple3d
-          end
-          if not PRO.cube_twosided then
-                    local Gen = Jkr.Generator(Jkr.Shapes.Cube3D, vec3(1, 1, 1))
-                    Gen = PRO.GetCubeGenerator(1, 1, 1, "TWOSIDED")
-                    PRO.cube_twosided = gshaper3d:Add(Gen, vec3(0, 0, 0))
-          end
-          if not PRO.cube_foursided then
-                    local Gen = Jkr.Generator(Jkr.Shapes.Cube3D, vec3(1, 1, 1))
-                    Gen = PRO.GetCubeGenerator(1, 1, 1, "FOURSIDED")
-                    PRO.cube_foursided = gshaper3d:Add(Gen, vec3(0, 0, 0))
-          end
+        local simple3did = gworld3d:AddSimple3D(Engine.i, gwindow)
+        local simple3d = gworld3d:GetSimple3D(simple3did)
+        simple3d:CompileEXT(
+            Engine.i,
+            gwindow,
+            "cache/3d_background_color_shader.glsl",
+            vshader.str,
+            fshader.str,
+            "",
+            false,
+            Jkr.CompileContext.Default
+        )
+        PRO.cube_ShowImageShader = {}
+        PRO.cube_ShowImageShader.simple3did = simple3did
+        PRO.cube_ShowImageShader.simple3d = simple3d
+    end
+    if not PRO.cube_twosided then
+        local Gen = PRO.GetCubeGenerator(0.5, 0.5, 0.5, "TWOSIDED")
+        PRO.cube_twosided = gshaper3d:Add(Gen, vec3(0, 0, 0))
+    end
+    if not PRO.cube_foursided then
+        local Gen = PRO.GetCubeGenerator(0.5, 0.5, 0.5, "FOURSIDED")
+        PRO.cube_foursided = gshaper3d:Add(Gen, vec3(0, 0, 0))
+    end
 end
